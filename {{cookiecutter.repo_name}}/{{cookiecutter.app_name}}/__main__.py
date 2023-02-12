448cc0473fd605263968695edef4ec154e2da0aa
puzzlepeaches <nanastasi@sprocketsecurity.com>
2023-02-12 08:48:49 -0600

    Big updates

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__main__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__main__.py
@@ -1,23 +1,9 @@
 #!/usr/bin/env python3
 
-import logging
-
 import click
 from click_extra import config_option
-from rich.console import Console
-from rich.logging import RichHandler
 
-# Setting up logging with rich
-FORMAT = "%(message)s"
-logging.basicConfig(
-    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(rich_tracebacks=True)]
-)
-
-log = logging.getLogger("rich")
-
-# Initializing console for rich
-console = Console()
-print = console.print
+from .utils.setup import log, print
 
 # Setting context settings for click
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help", "help"])
---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/utils/req.py
@@ -0,0 +1,34 @@
+from urllib.parse import urlparse
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+from .setup import log, print
+
+# Dealing with SSL Warnings
+try:
+    import requests.packages.urllib3
+    requests.packages.urllib3.disable_warnings()
+except Exception:
+    pass
+
+
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504, 503, 403),
+    session=None,
+):
+    session = session or requests.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+    return session
