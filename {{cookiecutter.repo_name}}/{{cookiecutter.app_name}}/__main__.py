b9a090b5735ae59653a45c301703d91215c211bb
puzzlepeaches <nanastasi@sprocketsecurity.com>
2024-09-10 05:43:34 -0500

    Refresh for blog

--- {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__main__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.app_name}}/__main__.py
@@ -9,6 +9,7 @@ from .utils.helpers import *
 # Setting context settings for click
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help", "help"])
 
+# Modify no_args_is_help to False to allow for no arguments to be passed to the command.
 @click.command(no_args_is_help=True, context_settings=CONTEXT_SETTINGS)
 @click.option("--debug", "-d", is_flag=True, help="Enable debug mode.")
 @config_option(default="config.yaml", help="Path to config file.")
